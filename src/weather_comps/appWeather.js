5e79b3b6692ea7f27f1167a9dcdbb41f0bf23a13
Chaya Segal <chayale90@gmail.com>
2022-10-20 00:04:43 +0300

    aaa

--- src/weather_comps/appWeather.js
+++ src/weather_comps/appWeather.js
@@ -4,13 +4,13 @@ import WeahterInfo from './weatherInfo'
 import WeatherInput from './weatherInput'
 
 export default function AppWeather() {
-  const [infoObj,setInfoObj] = useState({});
+  const [infoObj, setInfoObj] = useState({});
 
   useEffect(() => {
     doApi("Eilat")
-  },[])
+  }, [])
 
-  const doApi = async(_townName) => {
+  const doApi = async (_townName) => {
     let url = `https://api.openweathermap.org/data/2.5/weather?q=${_townName}&appid=3069ae2718e40f8dc1998b7250e16f10&units=metric`;
     let resp = await axios.get(url);
     console.log(resp.data);

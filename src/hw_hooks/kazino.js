e717681e193f469a2eb50e69ad5f5a66fb27e82f
Chaya Segal <chayale90@gmail.com>
2022-10-22 21:56:06 +0300

    aaa

--- src/hw_hooks/kazino.js
+++ src/hw_hooks/kazino.js
@@ -5,26 +5,37 @@ export default function Kazino() {
 
     const [coin, setCoin] = useState(5)
     const [counter, setCounter] = useState("")
-    
-    const arrDices = ["dice1.jpg","dice2.jpg","dice3.jpg","dice4.jpg","dice5.jpg","dice6.jpg"]
-    
+    const [gameOver, setgameOver] = useState(false)
+
+    const arrDices = ["dice1.jpg", "dice2.jpg", "dice3.jpg", "dice4.jpg", "dice5.jpg", "dice6.jpg"]
+
     const roll = () => {
         let rnd = Math.floor(Math.random() * 6);
+        console.log(rnd);
         setCounter(rnd)
         setCoin(coin - 1)
-        if(counter-1==4||counter-1==5){
-            setCoin(coin +2)
+
+        // אם עושים תנאי רגיל צריך לזכור שהאחריי הסט סטייט המשתנה בסטייט לא מעודכן
+        // עדיין בפונקציה עצמה
+        // if(this.state.counter+1 > 5){
+        //   this.setState({counter:0});
+        // }
+
+        if (counter >= 4) {
+            setCoin(coin + 2)
         }
-        console.log(coin);
+        if (coin + 1 <= 0) {
+            setgameOver(true)
+        }
     }
     return (
         <div className='container text-center p-3'>
             <h1 className='display-6'>Kazion Game</h1>
             <h2>coins: {coin}</h2>
-            <img src={"/images/"+arrDices[counter]}/>
+            <img src={"/images/" + arrDices[counter]} />
             <div>
-            <button onClick={roll} className='btn btn-info me-2'>Roll</button>
-        </div>
+                <button onClick={roll} className='btn btn-info me-2'>Roll</button>
+            </div>
         </div>
     )
 }

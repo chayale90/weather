10e3b3066d304c2681d1fc72325fb63c507d9141
Chaya Segal <chayale90@gmail.com>
2022-10-22 21:23:34 +0300

    aaa

--- src/hw_hooks/kazino.js
+++ src/hw_hooks/kazino.js
@@ -3,18 +3,20 @@ import React, { useState } from 'react'
 export default function Kazino() {
 
 
-    const [coin, setcoin] = useState(5)
+    const [coin, setCoin] = useState(5)
+    const [counter, setCounter] = useState(0)
 
-    const arDice = []
+    const arrDices = ["dice1.jpg","dice2.jpg","dice3.jpg","dice4.jpg","dice5.jpg","dice6.jpg"]
 
     const roll = () => {
-        setcoin(coin - 1)
+        setCoin(coin - 1)
     }
     return (
         <div className='container text-center p-3'>
             <h1 className='display-6'>Kazion Game</h1>
             <h2>{coin}</h2>
-            <button onClick={roll} className='btn btn-info me-2'>Roll </button>
+            <img src={"/images/"+arrDices[3]}/>
+            <button onClick={roll} className='btn btn-info me-2'>Roll</button>
         </div>
     )
 }

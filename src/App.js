9ecaf2ddcfd58c4d4f4d9cb9cc7d248a8828bb96
Chaya Segal <chayale90@gmail.com>
2022-10-24 00:45:47 +0300

    aaaa

--- src/App.js
+++ src/App.js
@@ -1,7 +1,4 @@
-import AppTv from './api_comps/appTv';
 import './App.css';
-import AppHooks from './hooks_comps/appHooks';
-import App_hwHooks from './hw_hooks/app_hwHooks';
 import AppWeather from './weather_comps/appWeather';
 
 

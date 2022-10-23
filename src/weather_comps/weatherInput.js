e7946cb0c63c10412ae9042cf238d5d8459f2848
Chaya Segal <chayale90@gmail.com>
2022-10-24 00:46:24 +0300

    aaa

--- src/weather_comps/weatherInput.js
+++ src/weather_comps/weatherInput.js
@@ -9,7 +9,7 @@ export default function WeatherInput(props) {
       <nav className="container">
 
         <div className='col-md-4 d-flex'>
-          <input ref={inputRef} type="text" className='form-control' placeholder='search for city/town...' />
+          <input ref={inputRef} type="text" className='form-control' placeholder='Search for city/town...' />
           <button onClick={() => {
             props.doApi(inputRef.current.value);
           }} className='btn btn-success ms-2'>Search</button>

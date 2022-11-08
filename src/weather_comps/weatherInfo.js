89d200f11c744a38bc5ece5106edbd06a6766533
Chaya Segal <chayale90@gmail.com>
2022-11-09 01:33:33 +0200

    aaa

--- src/weather_comps/weatherInfo.js
+++ src/weather_comps/weatherInfo.js
@@ -2,18 +2,30 @@ import React from 'react'
 
 export default function WeatherInfo(props) {
   let item = props.item
-
+  const myStyle={
+    backgroundImage: "url(images/img11.jpg)",
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: "center",
+    height: "578px"
+};
   return (
-    <div className='container text-center'>
+    <div className='container-fluid' style={myStyle}>
+
+
+    <div className='container text-center' >
       {(item.name) ? 
-      <div>
-        <h1 className='display-5 text-success'>Weather app</h1>
-        <h2>Weather of: {item.name}</h2>
+      <div className='p-2'>
+        <h2 className='display-3 text-light'>Weather app</h2>
+        <h1 className=' text-success'> {item.name}</h1>
         <img src={`http://openweathermap.org/img/w/${item.weather[0].icon}.png`} width="150" alt="icon" />
-        <div>condition: {item.weather[0].description}</div>
-        <div>Temp: {item.main?.temp} </div>
-        <div>Wind speed: {item.wind?.speed} kmh</div>
-      </div> : <h3>Loading...</h3>}
+        <h4>Condition: {item.weather[0].description}</h4>
+        <h4>Temp: {item.main?.temp} celius</h4>
+        <h4>Wind speed: {item.wind?.speed} kmh</h4>
+      </div> : <div style={{backgroundImage:"/images/hug.gif"}}></div>}
     </div>
+
+    </div>
+
   )
 }
